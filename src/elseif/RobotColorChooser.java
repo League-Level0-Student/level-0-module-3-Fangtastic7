ad6760d465a1dee654e222c2db16485fea9957d3
SunStar56 <talipetersen@gmail.com>
2017-11-17 18:31:24 -0800

    RobotColorChooser (IP)

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -11,9 +11,11 @@ public class RobotColorChooser {
 	public static void main(String[] args) {
 
 		//3. ask the user what color they would like the robot to draw
-		
+		String color = JOptionPane.showInputDialog("What color do you want the robot to draw?");
 		//4. use an if/else statement to set the pen color that the user requested
-
+		if(color.equals("red")) {
+			
+		}
         //5. if the user doesn’t enter anything, choose a random color
 
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
