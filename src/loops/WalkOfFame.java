3bc016c5425486de70339aca7198fca3fdb109fe
zahmed2003 <league@WTS10.local>
2017-12-08 18:26:40 -0800

    Walk of Fame, FizzBuzz (ip)

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,23 +11,32 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-		rob.setX(0);
+		rob.setX(100);
+		rob.setSpeed(1000);
+		
+		for(int i = 0 ; i < 30; i++) {
+		rob.penDown();
 		// 2. Make the robot draw a star shape. Hint: 144.
-		for (int i = 0; i < 5; i++) {
+		for (int t = 0; t < 5; t++) {
+			
 			rob.move(30);
 			rob.turn(144);
 		}
-		
+		rob.hide();
 		// 3. Set the size of the star to 30.
-		rob.
+		
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
 		 * http://bit.ly/walk-of-fame
 		 * 
 		 * Hint: The distance between stars is 50.
+		 
 		 */
-
+		rob.penUp();
+		rob.setX(100 + i*50);
+	
+		}
 	}
 
 }
